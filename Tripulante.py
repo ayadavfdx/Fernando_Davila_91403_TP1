aac3522ed83ecf495b15bc2d745eb3ae72f4512a
ayadavfdx <blitzan05@hotmail.com>
2026-02-20 23:34:38 +0000

    Added __validate_range(...) for energy and power | Used colorama to print

--- Tripulante.py
+++ Tripulante.py
@@ -6,15 +6,22 @@ from colorama import init
 init()
 
 #Define Class
-class Tripulantes():
+class Tripulante():
     def __init__(self,name,role,bounty,power,energy):
-        self.__name=name
-        self.__role=role
-        self.__bounty=bounty
-        self.__power=power
-        self.__energy=energy
+        self.__name=str(name)
+        self.__role=str(role)
+        self.__bounty=float(bounty)
+        self.power=int(power)
+        self.__energy=int(energy)
 
-    #Use getters and setters
+    #Function to validate range to 0-100
+    def __validate_range(self,value):
+        if value <0 or value > 100:
+            print(Fore.RED + "must be a value between 0-100".upper() + Style.RESET_ALL)
+        else:
+            return value
+        
+    #Use getters, make private attributes
     @property
     def name(self):
         return self.__name
@@ -31,6 +38,17 @@ class Tripulantes():
     def power(self):
         return self.__power
     
+    #Use a setter to validate range 0-100
+    @power.setter
+    def power(self,value):
+        self.__power= self.__validate_range(value)
+
     @property
     def energy(self):
         return self.__energy
+    
+    #Use a setter to validate range 0-100
+    @energy.setter
+    def energy(self,value):
+        self.__energy= self.__validate_range(value)
+
