7794e249407d168c939e099980daab81017a5e1d
ayadavfdx <blitzan05@hotmail.com>
2026-03-01 22:32:41 +0000

    Fixed language problems

--- tp1_p1_main.py
+++ tp1_p1_main.py
@@ -22,9 +22,9 @@ def space():
 
 #Test create a crew member
 title("Create-Crew-Members (tripulante)".upper())
-p1= Tripulante("Luffy","Captain",1300.65,100,100)
-p2= Tripulante("Sanji","Chef",3010.3,100,70)
-p3= Tripulante("Ussop","Sniper",3250.65,100,80)
+p1= Tripulante("Luffy",1300.65,100,100)
+p2= Tripulante("Sanji",3010.3,100,70)
+p3= Tripulante("Ussop",3250.65,100,80)
 print(p1)
 print(p2)
 print(p3)
@@ -32,9 +32,9 @@ space()
 
 #Test work
 title("TEST-WORK")
-p1.work(5)
-p2.work(3)
-p3.work(10)
+p1.trabalhar(5)
+p2.trabalhar(3)
+p3.trabalhar(10)
 print(p1)
 print(p2)
 print(p3)
@@ -42,9 +42,9 @@ space()
 
 #Test Rest
 title("TEST-REST")
-p1.rest()
-p2.rest()
-p3.rest()
+p1.descansar()
+p2.descansar()
+p3.descansar()
 print(p1)
 print(p2)
 print(p3)
@@ -53,25 +53,25 @@ space()
 
 #Test Create Navio
 title("Create-Navio".upper())
-navio= Navio("Going Marry",[p1,p2,p3])
-navio.show_manifesto()
+navio= Navio("Going Marry",[p1,p2,p3],100)
+navio.mostrar_manifesto()
 space()
 
 #Test Recruit
 title("TEST-RECRUIT-CREW-MEMBER")
 p4= Tripulante("Nami","Navigator",389.43,88,50)
-navio.recruit(p4)
+navio.recrutar(p4)
 #p5= Tripulante("Nami","Janitor",563.23,85,30)
 #navio.recruit(p5)
-navio.show_manifesto()
-print(f"{Fore.LIGHTWHITE_EX}New member recluted: {navio.crew[-1]} ")
+navio.mostrar_manifesto()
+print(f"{Fore.LIGHTWHITE_EX}New member recluted: {navio.tripulacao[-1]} ")
 space()
 
 #Test Kick
 title("TEST-KICK-CREW-MEMBER")
-navio.show_manifesto()
-status=navio.kick("Nami")
-#status=navio.kick("Nami")
+navio.mostrar_manifesto()
+status=navio.expulsar("Nami")
+#status=navio.expulsar("Nami")
 
 if status == True:
     print(f"{Fore.LIGHTWHITE_EX}Kick Status: {Fore.LIGHTGREEN_EX}{status} ")
@@ -82,14 +82,14 @@ space()
 
 #Test total Power
 title("TEST-TOTAL-POWER")
-navio.show_manifesto()
-print(f"{Fore.LIGHTWHITE_EX}Total Power: {Fore.LIGHTYELLOW_EX}{navio.total_power()} ")
+navio.mostrar_manifesto()
+print(f"{Fore.LIGHTWHITE_EX}Total Power: {Fore.LIGHTYELLOW_EX}{navio.calcular_poder_total()} ")
 space()
 
 #Show final manifesto
 title("MANIFESTO")
-navio.show_manifesto()
-print(f"{Fore.LIGHTWHITE_EX}Total Power: {Fore.LIGHTYELLOW_EX}{navio.total_power()} ")
+navio.mostrar_manifesto()
+print(f"{Fore.LIGHTWHITE_EX}Total Power: {Fore.LIGHTYELLOW_EX}{navio.calcular_poder_total()} ")
 
 
 
