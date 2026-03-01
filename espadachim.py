ced32833750aff76af54fe33beb2d7f1f66ed420
ayadavfdx <blitzan05@hotmail.com>
2026-03-01 17:57:03 +0000

    Fixed syntax error line 11 | Override __str__()

--- espadachim.py
+++ espadachim.py
@@ -1,9 +1,14 @@
 
 #Import all necessary
 from tripulante import Tripulante
+from colorama import Style,Fore,init
+
+
+#Initialize colorama
+init(autoreset=True)
 
 class Espadachim(Tripulante):
-    def __init__(self,espadas=[]):
+    def __init__(self,name,bounty,power,energy,espadas=[]):
         super().__init__(name,bounty,power,energy)
         self.__espadas=list(espadas)
 
@@ -12,4 +17,6 @@ class Espadachim(Tripulante):
         return self.__espadas
     
     def __str__(self):
-        return super().__str__()
+        info= super().__str__()
+        return (f"{info} {Fore.LIGHTWHITE_EX}|Swords:{Style.RESET_ALL} {self.random_colors()}{', '.join(self.__espadas)}")
+
